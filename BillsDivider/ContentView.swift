6eda5fc48f2fd31e4c5f57f2058b54c59ca5abd2
Adrian Świątek <adrian@aswiatek.pl>
2019-10-20 21:39:57 +0200

    Initial Commit

---
+++ BillsDivider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BillsDivider
+//
+//  Created by Adrian Świątek on 20/10/2019.
+//  Copyright © 2019 Adrian Świątek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
