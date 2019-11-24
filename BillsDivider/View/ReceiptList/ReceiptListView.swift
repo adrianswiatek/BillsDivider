662018b6a40cf1dc6c5d989d53ada6ab43d95850
Adrian Świątek <adrian@aswiatek.pl>
2019-11-24 01:18:03 +0100

    Added edit bar button item to list screen.

--- BillsDivider/View/ReceiptList/ReceiptListView.swift
+++ BillsDivider/View/ReceiptList/ReceiptListView.swift
@@ -24,10 +24,17 @@ struct ReceiptListView: View {
                 }
             }
             .navigationBarTitle(Text(""), displayMode: .inline)
-            .navigationBarItems(trailing: Button(action: { self.presentingAddOverlay = true }) {
-                Image(systemName: "plus")
-                    .frame(width: 32, height: 32)
-            })
+            .navigationBarItems(
+                leading: Button(action: { }) {
+                    Image(systemName: "ellipsis")
+                        .frame(width: 32, height: 32)
+                        .rotationEffect(.degrees(90))
+                },
+                trailing: Button(action: { self.presentingAddOverlay = true }) {
+                    Image(systemName: "plus")
+                        .frame(width: 32, height: 32)
+                }
+            )
         }
         .sheet(isPresented: $presentingAddOverlay) {
             self.createAddOverlayView()
