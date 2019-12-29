92f1a5660eb5294bf9267a2b1e5d80c9bb577bce
Adrian Świątek <adrian@aswiatek.pl>
2019-12-29 17:38:06 +0100

    Updated DividerTests.

--- BillsDividerTests/Cases/Model/DividerTests.swift
+++ BillsDividerTests/Cases/Model/DividerTests.swift
@@ -3,194 +3,221 @@ import XCTest
 
 class DividerTests: XCTestCase {
     private var sut: Divider!
+    private var people: [Person]!
+
+    private var firstPerson: Person {
+        people[0]
+    }
+
+    private var secondPerson: Person {
+        people[1]
+    }
 
     override func setUp() {
         super.setUp()
+        people = [.withGeneratedName(forNumber: 1), .withGeneratedName(forNumber: 2)]
         sut = Divider()
     }
 
     override func tearDown() {
         sut = nil
+        people = nil
         super.tearDown()
+    }
+
+    func testDivide_withPeopleCountLessThanTwo_returnsNoDebtResult() {
+        let resultForZeroPeople = sut.divide([], between: [])
+        XCTAssertEqual(resultForZeroPeople, .noDebt)
+
+        let resultForOnePerson = sut.divide([], between: [.withGeneratedName(forNumber: 1)])
+        XCTAssertEqual(resultForOnePerson, .noDebt)
     }
 
     func testDivide_withEmptyList_returnNoDebtResult() {
         let positions: [ReceiptPosition] = []
-        let result = sut.divide(positions)
+        let result = sut.divide(positions, between: people)
         XCTAssertEqual(result, .noDebt)
     }
 
     func testDivide_withEmptyPosition_returnsNoDebtResult() {
         let positions: [ReceiptPosition] = [.empty]
-        let result = sut.divide(positions)
+        let result = sut.divide(positions, between: people)
         XCTAssertEqual(result, .noDebt)
     }
 
     func testDivide_withZeroAmountPositions_returnNoDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 0, buyer: .me, owner: .me),
-            .init(amount: 0, buyer: .me, owner: .notMe),
-            .init(amount: 0, buyer: .notMe, owner: .me),
-            .init(amount: 0, buyer: .notMe, owner: .notMe)
+            .init(amount: 0, buyer: .person(firstPerson), owner: .person(firstPerson)),
+            .init(amount: 0, buyer: .person(firstPerson), owner: .person(secondPerson)),
+            .init(amount: 0, buyer: .person(secondPerson), owner: .person(firstPerson)),
+            .init(amount: 0, buyer: .person(secondPerson), owner: .person(secondPerson))
         ]
-        let result = sut.divide(positions)
+        let result = sut.divide(positions, between: people)
         XCTAssertEqual(result, .noDebt)
     }
 
     func testDivide_withBuyerAndOwnerTheSame_returnsNoDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 0, buyer: .me, owner: .me),
-            .init(amount: 1, buyer: .me, owner: .me),
-            .init(amount: 0, buyer: .notMe, owner: .notMe),
-            .init(amount: 1, buyer: .notMe, owner: .notMe)
+            .init(amount: 0, buyer: .person(firstPerson), owner: .person(firstPerson)),
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(firstPerson)),
+            .init(amount: 0, buyer: .person(secondPerson), owner: .person(secondPerson)),
+            .init(amount: 1, buyer: .person(secondPerson), owner: .person(secondPerson))
         ]
-        let result = sut.divide(positions)
+        let result = sut.divide(positions, between: people)
         XCTAssertEqual(result, .noDebt)
     }
 
-    func testDivide_withBuyerSetToMeAndOwnerSetToNotMe_returnsDebtResult() {
-        let positions: [ReceiptPosition] = [.init(amount: 1, buyer: .me, owner: .notMe)]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 1))
-    }
-
-    func testDivide_withBuyerSetToNotMeAndOwnerSetToMe_returnsDebtResult() {
-        let positions: [ReceiptPosition] = [.init(amount: 1, buyer: .notMe, owner: .me)]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .notMe, debtor: .me, amount: 1))
-    }
-
-    func testDivide_withBuyerSetToMeAndOwnerSetToAll_returnsDebtResult() {
-        let positions: [ReceiptPosition] = [.init(amount: 1, buyer: .me, owner: .all)]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 0.5))
-    }
-
-    func testDivide_withBuyerSetToNotMeAndOwnerSetToAll_returnsDebtResult() {
-        let positions: [ReceiptPosition] = [.init(amount: 1, buyer: .notMe, owner: .all)]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .notMe, debtor: .me, amount: 0.5))
-    }
-
-    func testDivide_debtAndNoDebtItemsWithBuyerSetToMeAndOwnerSetToNotMe_returnsDebtResult() {
+    func testDivide_withBuyerSetToFirstPersonAndOwnerSetToSecondPerson_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .notMe),
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(secondPerson))
+        ]
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 1))
+    }
+
+    func testDivide_withBuyerSetToSecondPersonAndOwnerSetToFirstPerson_returnsDebtResult() {
+        let positions: [ReceiptPosition] = [
+            .init(amount: 1, buyer: .person(secondPerson), owner: .person(firstPerson))
+        ]
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(secondPerson), debtor: .person(firstPerson), amount: 1))
+    }
+
+    func testDivide_withBuyerSetToFirstPersonAndOwnerSetToAll_returnsDebtResult() {
+        let positions: [ReceiptPosition] = [
+            .init(amount: 1, buyer: .person(firstPerson), owner: .all)
+        ]
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 0.5))
+    }
+
+    func testDivide_withBuyerSetToSecondPersonAndOwnerSetToAll_returnsDebtResult() {
+        let positions: [ReceiptPosition] = [
+            .init(amount: 1, buyer: .person(secondPerson), owner: .all)
+        ]
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(secondPerson), debtor: .person(firstPerson), amount: 0.5))
+    }
+
+    func testDivide_debtAndNoDebtItemsWithBuyerSetToFirstPersonAndOwnerSetToSecondPerson_returnsDebtResult() {
+        let positions: [ReceiptPosition] = [
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(secondPerson)),
             .empty
         ]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 1))
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 1))
     }
 
-    func testDivide_noDebtAndDebtItemsWithbuyerSetToMeAndOwnerSetToNotMe_returnsDebtResult() {
+    func testDivide_noDebtAndDebtItemsWithbuyerSetToFirstPersonAndOwnerSetToSecondPerson_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
             .empty,
-            .init(amount: 1, buyer: .me, owner: .notMe)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(secondPerson))
         ]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 1))
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 1))
     }
 
-    func testDivide_twoDebtItemsWithBuyerSetToMeAndOwnerSetToNotMe_returnsDebtResult() {
+    func testDivide_twoDebtItemsWithBuyerSetToFirstPersonAndOwnerSetToSecondPerson_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .notMe),
-            .init(amount: 2, buyer: .me, owner: .notMe)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(secondPerson)),
+            .init(amount: 2, buyer: .person(firstPerson), owner: .person(secondPerson))
         ]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 3))
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 3))
     }
 
-    func testDivide_twoDebtItemsWithBuyerSetToMeAndOwnerSetToAll_returnsDebtResult() {
+    func testDivide_twoDebtItemsWithBuyerSetToFirstPersonAndOwnerSetToAll_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .all),
-            .init(amount: 2, buyer: .me, owner: .all)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .all),
+            .init(amount: 2, buyer: .person(firstPerson), owner: .all)
         ]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 1.5))
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 1.5))
     }
 
     func testDivide_twoDebtItemsWithMixedBuyerOwnerAndAmount_returnsDebtResult() {
         var positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .notMe),
-            .init(amount: 2, buyer: .notMe, owner: .me)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(secondPerson)),
+            .init(amount: 2, buyer: .person(secondPerson), owner: .person(firstPerson))
         ]
-        var result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .notMe, debtor: .me, amount: 1))
+        var result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(secondPerson), debtor: .person(firstPerson), amount: 1))
 
         positions = [
-            .init(amount: 2, buyer: .me, owner: .notMe),
-            .init(amount: 1, buyer: .notMe, owner: .me)
+            .init(amount: 2, buyer: .person(firstPerson), owner: .person(secondPerson)),
+            .init(amount: 1, buyer: .person(secondPerson), owner: .person(firstPerson))
         ]
-        result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 1))
+        result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 1))
     }
 
     func testDivide_twoDebtItemsWithMixedBuyerOwnerAndEqualAmount_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .notMe),
-            .init(amount: 1, buyer: .notMe, owner: .me)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(secondPerson)),
+            .init(amount: 1, buyer: .person(secondPerson), owner: .person(firstPerson))
         ]
-        let result = sut.divide(positions)
+        let result = sut.divide(positions, between: people)
         XCTAssertEqual(result, .noDebt)
     }
 
     func testDivide_twoDebtItemsWithMixedBuyerOwnerSetToAllAndEqualAmount_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .all),
-            .init(amount: 1, buyer: .notMe, owner: .all)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .all),
+            .init(amount: 1, buyer: .person(secondPerson), owner: .all)
         ]
-        let result = sut.divide(positions)
+        let result = sut.divide(positions, between: people)
         XCTAssertEqual(result, .noDebt)
     }
 
     func testDivide_twoDebtItemsWithMixedBuyerOwnerSetToAllAndDifferentAmount_returnsDebtResult() {
         var positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .all),
-            .init(amount: 2, buyer: .notMe, owner: .all)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .all),
+            .init(amount: 2, buyer: .person(secondPerson), owner: .all)
         ]
-        var result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .notMe, debtor: .me, amount: 0.5))
+        var result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(secondPerson), debtor: .person(firstPerson), amount: 0.5))
 
         positions = [
-            .init(amount: 2, buyer: .me, owner: .all),
-            .init(amount: 1, buyer: .notMe, owner: .all)
+            .init(amount: 2, buyer: .person(firstPerson), owner: .all),
+            .init(amount: 1, buyer: .person(secondPerson), owner: .all)
         ]
-        result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 0.5))
+        result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 0.5))
     }
 
-    func testDivide_twoDebtItemsWithBuyerSetToMeMixedOwnersAndEqualAmount_returnsDebtResult() {
+    func testDivide_twoDebtItemsWithBuyerSetToFirstPersonMixedOwnersAndEqualAmount_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .notMe),
-            .init(amount: 1, buyer: .me, owner: .all)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(secondPerson)),
+            .init(amount: 1, buyer: .person(firstPerson), owner: .all)
         ]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 1.5))
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 1.5))
     }
 
-    func testDivide_twoDebtItemsWithBuyerSetToMeMixedOwnersAndDifferentAmount_returnsDebtResult() {
+    func testDivide_twoDebtItemsWithBuyerSetToFirstPersonMixedOwnersAndDifferentAmount_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .me, owner: .notMe),
-            .init(amount: 2, buyer: .me, owner: .all)
+            .init(amount: 1, buyer: .person(firstPerson), owner: .person(secondPerson)),
+            .init(amount: 2, buyer: .person(firstPerson), owner: .all)
         ]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .me, debtor: .notMe, amount: 2))
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(firstPerson), debtor: .person(secondPerson), amount: 2))
     }
 
     func testDivide_twoDebtItemsWithBuyerSetToNotMeMixedOwnersAndEqualAmount_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .notMe, owner: .me),
-            .init(amount: 1, buyer: .notMe, owner: .all)
+            .init(amount: 1, buyer: .person(secondPerson), owner: .person(firstPerson)),
+            .init(amount: 1, buyer: .person(secondPerson), owner: .all)
         ]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .notMe, debtor: .me, amount: 1.5))
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(secondPerson), debtor: .person(firstPerson), amount: 1.5))
     }
 
     func testDivide_twoDebtItemsWithBuyerSetToNotMeMixedOwnersAndDifferentAmount_returnsDebtResult() {
         let positions: [ReceiptPosition] = [
-            .init(amount: 1, buyer: .notMe, owner: .me),
-            .init(amount: 2, buyer: .notMe, owner: .all)
+            .init(amount: 1, buyer: .person(secondPerson), owner: .person(firstPerson)),
+            .init(amount: 2, buyer: .person(secondPerson), owner: .all)
         ]
-        let result = sut.divide(positions)
-        XCTAssertEqual(result, .debt(lender: .notMe, debtor: .me, amount: 2))
+        let result = sut.divide(positions, between: people)
+        XCTAssertEqual(result, .debt(lender: .person(secondPerson), debtor: .person(firstPerson), amount: 2))
     }
 }
