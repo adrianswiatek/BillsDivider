6c75a6fbc7b98780a7c87c8bc9e9dd17debe8634
Adrian Świątek <adrian@aswiatek.pl>
2019-12-30 22:23:04 +0100

    Updated DivisionResultTests.

--- BillsDividerTests/Cases/ViewModel/DivisionResultTests.swift
+++ BillsDividerTests/Cases/ViewModel/DivisionResultTests.swift
@@ -7,49 +7,62 @@ class DivisionResultTests: XCTestCase {
         XCTAssertEqual(sut.debtAmount, 0)
     }
 
+    private func buyer(_ number: Int) -> Buyer {
+        .person(.withGeneratedName(forNumber: number))
+    }
+
     func testDebtAmount_debtCase_returnsGivenAmount() {
-        let zeroAmountCase: DivisionResult = .debt(lender: .me, debtor: .me, amount: 0)
+        let zeroAmountCase: DivisionResult = .debt(lender: buyer(1), debtor: buyer(1), amount: 0)
         XCTAssertEqual(zeroAmountCase.debtAmount, 0)
 
-        let oneAmountCase: DivisionResult = .debt(lender: .me, debtor: .me, amount: 1)
+        let oneAmountCase: DivisionResult = .debt(lender: buyer(1), debtor: buyer(1), amount: 1)
         XCTAssertEqual(oneAmountCase.debtAmount, 1)
 
-        let oneFifthyCase: DivisionResult = .debt(lender: .me, debtor: .me, amount: 1.5)
+        let oneFifthyCase: DivisionResult = .debt(lender: buyer(1), debtor: buyer(1), amount: 1.5)
         XCTAssertEqual(oneFifthyCase.debtAmount, 1.5)
     }
 
-    func testEqualsOperator_twoNoDebtCases_returnsTrue() {
+    func testEquals_twoNoDebtCases_returnsTrue() {
         XCTAssertTrue(DivisionResult.noDebt == DivisionResult.noDebt)
     }
 
-    func testEqualsOperator_oneNoDebtCaseAndOneDebtCase_returnsFalse() {
+    func testEquals_oneNoDebtCaseAndOneDebtCase_returnsFalse() {
         let first: DivisionResult = .noDebt
-        let second: DivisionResult = .debt(lender: .me, debtor: .me, amount: 0)
+        let second: DivisionResult = .debt(lender: buyer(1), debtor: buyer(1), amount: 0)
+
         XCTAssertFalse(first == second)
         XCTAssertFalse(second == first)
     }
 
     func testEqualsOperator_twoIdenticalNoDebtCases_returnsTrue() {
-        let first: DivisionResult = .debt(lender: .me, debtor: .me, amount: 0)
-        let second: DivisionResult = .debt(lender: .me, debtor: .me, amount: 0)
+        let first: DivisionResult = .debt(lender: buyer(1), debtor: buyer(1), amount: 0)
+        let second: DivisionResult = .debt(lender: buyer(1), debtor: buyer(1), amount: 0)
+
         XCTAssertTrue(first == second)
+        XCTAssertTrue(second == first)
     }
 
     func testEqualsOperator_twoNoDebtCasesWithDifferentLenders_returnsFalse() {
-        let first: DivisionResult = .debt(lender: .me, debtor: .me, amount: 0)
-        let second: DivisionResult = .debt(lender: .notMe, debtor: .me, amount: 0)
+        let first: DivisionResult = .debt(lender:  buyer(1), debtor: buyer(1), amount: 0)
+        let second: DivisionResult = .debt(lender: buyer(2), debtor: buyer(1), amount: 0)
+
         XCTAssertFalse(first == second)
+        XCTAssertFalse(second == first)
     }
 
     func testEqualsOperator_twoNoDebtCasesWithDifferentDebtors_returnsFalse() {
-        let first: DivisionResult = .debt(lender: .me, debtor: .me, amount: 0)
-        let second: DivisionResult = .debt(lender: .me, debtor: .notMe, amount: 0)
+        let first: DivisionResult = .debt(lender:  buyer(1), debtor: buyer(1), amount: 0)
+        let second: DivisionResult = .debt(lender: buyer(1), debtor: buyer(2), amount: 0)
+
         XCTAssertFalse(first == second)
+        XCTAssertFalse(second == first)
     }
 
     func testEqualsOperator_twoNoDebtCasesWithDifferentAmount_returnsFalse() {
-        let first: DivisionResult = .debt(lender: .me, debtor: .me, amount: 0)
-        let second: DivisionResult = .debt(lender: .me, debtor: .me, amount: 1)
+        let first: DivisionResult = .debt(lender:  buyer(1), debtor: buyer(1), amount: 0)
+        let second: DivisionResult = .debt(lender: buyer(1), debtor: buyer(1), amount: 1)
+
         XCTAssertFalse(first == second)
+        XCTAssertFalse(second == first)
     }
 }
