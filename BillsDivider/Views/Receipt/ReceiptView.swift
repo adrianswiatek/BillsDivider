88ddf763be8984341c974f7f6ae13377a99ba04c
Adrian Świątek <adrian@aswiatek.pl>
2020-03-26 20:53:05 +0100

    Fixed navigation view for Xcode 11.4.

--- BillsDivider/Views/Receipt/ReceiptView.swift
+++ BillsDivider/Views/Receipt/ReceiptView.swift
@@ -4,58 +4,58 @@ import SwiftUI
 
 struct ReceiptView: View {
     @ObservedObject private var viewModel: ReceiptViewModel
-
+    
     @State private var editOverlayParams: EditOverlayViewParams = .hidden
     @State private var presentingOptionsMenu: Bool = false
-
+    
     private let editOverlayViewFactory: EditOverlayViewFactory
     private let columnWidth: CGFloat = UIScreen.main.bounds.width / 3
-
+    
     init(_ viewModel: ReceiptViewModel, _ editOverlayViewFactory: EditOverlayViewFactory) {
         self.viewModel = viewModel
         self.editOverlayViewFactory = editOverlayViewFactory
     }
-
+    
     var body: some View {
         NavigationView {
             List {
                 Section(header: ReceiptHeaderView()) {
                     ForEach(viewModel.positions) { position in
-                            self.row(forPosition: position)
-                                .contextMenu {
-                                    Button(action: { self.editOverlayParams = .shownEditing(position) }) {
-                                        Text("Edit position")
-                                        Image(systemName: "pencil")
-                                    }
-                                    Button(action: { self.viewModel.removePosition(position) }) {
-                                        Text("Delete position")
-                                        Image(systemName: "trash")
-                                    }
+                        self.row(forPosition: position)
+                            .contextMenu {
+                                Button(action: { self.editOverlayParams = .shownEditing(position) }) {
+                                    Text("Edit position")
+                                    Image(systemName: "pencil")
+                                }
+                                Button(action: { self.viewModel.removePosition(position) }) {
+                                    Text("Delete position")
+                                    Image(systemName: "trash")
                                 }
                         }
-                        .onDelete {
-                            guard let index = $0.first else { return }
+                    }
+                    .onDelete {
+                        guard let index = $0.first else { return }
 
-                            withAnimation {
-                                self.viewModel.removePosition(at: index)
-                            }
+                        withAnimation {
+                            self.viewModel.removePosition(at: index)
                         }
                     }
                 }
-                .navigationBarTitle(Text(""), displayMode: .inline)
-                .navigationBarItems(
-                    leading: Button(action: { self.presentingOptionsMenu = true }) {
-                        Image(systemName: "ellipsis")
-                            .frame(width: 32, height: 32)
-                            .rotationEffect(.degrees(90))
-                    }
-                    .disabled(viewModel.ellipsisModeDisabled),
-                    trailing: Button(action: { self.editOverlayParams = .shownAdding() }) {
-                        Image(systemName: "plus")
-                            .frame(width: 32, height: 32)
-                    }
-                )
-                .accessibility(identifier: "ReceiptView.receiptPositions")
+            }
+            .navigationBarTitle(Text(""), displayMode: .inline)
+            .navigationBarItems(
+                leading: Button(action: { self.presentingOptionsMenu = true }) {
+                    Image(systemName: "ellipsis")
+                        .frame(width: 32, height: 32)
+                        .rotationEffect(.degrees(90))
+                }
+                .disabled(viewModel.ellipsisModeDisabled),
+                trailing: Button(action: { self.editOverlayParams = .shownAdding() }) {
+                    Image(systemName: "plus")
+                        .frame(width: 32, height: 32)
+                }
+            )
+            .accessibility(identifier: "ReceiptView.receiptPositions")
         }
         .sheet(isPresented: $editOverlayParams.show) {
             ZStack {
@@ -130,7 +130,7 @@ struct ReceiptView: View {
             .background(
                 RoundedRectangle(cornerRadius: 8)
                     .foregroundColor(.init(.init(white: 0.5, alpha: 1)))
-            )
+        )
             .offset(.init(width: 0, height: -UIScreen.main.bounds.height / 3))
     }
 }
--- BillsDivider/Views/Tabs/TabsView.swift
+++ BillsDivider/Views/Tabs/TabsView.swift
@@ -16,7 +16,6 @@ struct TabsView: View {
                 }
             }
         }
-        .edgesIgnoringSafeArea(.top)
     }
 }
 
